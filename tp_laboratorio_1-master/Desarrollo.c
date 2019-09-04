57e99482bf18b0d5d34e650196b1d59432b60b9f
Gabriel.Sosa <52495050+sosagab110697@users.noreply.github.com>
2019-09-04 18:34:56 -0300

    Add files via upload

---
+++ tp_laboratorio_1-master/Desarrollo.c
@@ -0,0 +1,38 @@
+//aca va el Desarrollo
+
+#include "Prototipo.h"
+//desarrollo
+
+int suma(int numero1,int numero2){
+    int resultado;
+    resultado = numero1 + numero2;
+    return resultado;
+}
+
+int resta(int numero1,int numero2){
+    int resultado;
+    resultado = numero1 - numero2;
+    return resultado;
+}
+
+float division (int numero1, int numero2){
+    float resultado;
+    resultado = (float)numero1 / numero2;
+    return resultado;
+}
+
+int multiplicacion(int numero1, int numero2){
+    int resultado;
+    resultado = numero1 * numero2;
+    return resultado;
+}
+
+int factorial(int numero1){
+    int resultado=1;
+    int i;
+
+for(i=numero1;i!=0;i--){
+    resultado = resultado * i;
+}
+return(resultado);
+}
---
+++ tp_laboratorio_1-master/Prototipo.h
@@ -0,0 +1,52 @@
+#include <stdio.h>
+//prototipo
+
+
+int suma(int,int);
+/** \brief suma 2 int y devuelve un int
+ *
+ * \param int recibe un int
+ * \param int recibe un int
+ * \return int devuelve un int sumado
+ *
+ */
+
+
+int resta(int,int);
+/** \brief resta 2 int y devuelve un int
+ *
+ * \param int recibe un int
+ * \param int recibe un int
+ * \return int devuelve un int restado
+ *
+ */
+
+
+ float division(int,int);
+/** \brief divide 2 int y puede q devuelva un float o un int
+ *
+ * \param int recibe un int
+ * \param int recibe un int
+ * \return float devuelve un float dividido
+ *
+ */
+
+
+ int multiplicacion(int,int);
+/** \brief multiplica 2 int y devuelve un int
+ *
+ * \param int recibe un int
+ * \param int recibe un int
+ * \return int devuelve un int multiplicado
+ *
+ */
+
+
+int factorial(int);
+/** \brief factoriza un int y devuelve un int
+ *
+ * \param int recibe un int
+ * \return int devuelve un int factorizado
+ *
+ */
+
